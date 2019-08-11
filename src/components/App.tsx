e42c47389cfe0024557850ee8ca3a6df35c1da93
Dongyu Kang <dongyu@dongyukang.com>
2019-08-11 15:00:54 +0900

    some commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,26 +1,9 @@
-import * as firebase from 'firebase';
 import React, { Component } from 'react';
 import Navigation from './navigation/Navigation';
 import { Home, Clients, Login, Projects, NotFound } from './pages'; // page components
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-    constructor(props: any) {
-        super(props);
-
-        var firebaseConfig = {
-            apiKey: process.env.REACT_APP_API_KEY,
-            authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-            databaseURL: process.env.REACT_APP_DATABASE_URL,
-            projectId: process.env.REACT_APP_PROJECT_ID,
-            storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-            messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-            appId: process.env.REACT_APP_APP_ID 
-        }
-
-        firebase.initializeApp(firebaseConfig);
-    }
-
     render() {
         return(
             <div>
